26fac096c26aeef9c0cf1f5f72efe0d478535258
Amine <aminemastouri@gmail.com>
2020-04-10 18:17:26 +0200

    add twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -21,6 +21,15 @@ class ArticleController extends AbstractController
      */
     public function show($slug)
     {
-        return new Response('future page to show the article:' . $slug);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
